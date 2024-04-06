cfab540cbe0a11b14f2690a0afa0a9c7a8d81606
dsgoryachev <ds.goryachev@gmail.com>
2024-04-06 19:02:43 +0300

    Unusefull class AdminChecker has just deleted

--- Main/Config.cs
+++ Main/Config.cs
@@ -7,10 +7,9 @@ namespace MessageGeneratorBot.Main
 
         internal const string TELEGRAM_LOGS_GROUP = "@GROUP_NAME";
 
-        internal const string PATH_TO_MAIN_FOLDER = @"C:\Users\d_goryachev\Desktop\1\_Visual Studio\UpsideBot\";
+        internal const string PATH_TO_MAIN_FOLDER = @"C:\Users\d_goryachev\Desktop\1\_Visual Studio\Bot\";
         internal const string PATH_TO_FOLDER_USERS_INFO = PATH_TO_MAIN_FOLDER + @"UsersInfo\";
         internal const string PATH_TO_CASH_FILE = PATH_TO_MAIN_FOLDER + "Cash.txt";
-        internal const string PATH_TO_LIST_OF_ADMINS_FILE = PATH_TO_MAIN_FOLDER + "ListOfAdmins.txt";
 
         internal const string ERROR_FEEDBACK = "\nPlease, push command /start or if something is still wrong write to @dsgor";
         internal const string ERROR_THERE_IS_NO_USER_IN_DATABASE = "There is no such user in database." + ERROR_FEEDBACK;
